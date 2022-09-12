b4af41e23e21312c2342bf1d78428bba6548fef9
Artak1999 <dashyan.1999@mail.ru>
2022-09-12 12:57:38 +0500

    Dish order is unclear

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,28 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
+        if(firstQueue.size() == 2 && secondQueue.size() == 2) {
+            arrayDeque.addAll(firstQueue);
+            arrayDeque.addAll(secondQueue);
+        }
+        else {
+            arrayDeque.add(firstQueue.remove());
+            arrayDeque.add(firstQueue.remove());
+            arrayDeque.add(secondQueue.remove());
+            arrayDeque.add(secondQueue.remove());
+            while(!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+                firstQueue.add(arrayDeque.removeLast());
+                arrayDeque.add(firstQueue.poll());
+                arrayDeque.add(firstQueue.poll());
+                secondQueue.add(arrayDeque.removeLast());
+                arrayDeque.add(secondQueue.poll());
+                arrayDeque.add(secondQueue.poll());
+            }
+        }
+        return arrayDeque;
     }
 }
